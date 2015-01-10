356d3ac30310010b81b2adc88d0b982471dd97b4
dsmilyanov <dsmilyanov@gmail.com>
2015-01-11 00:45:04 +0200

    Added controller for publishing with according services

---
+++ app/js/services/pageService.js
@@ -0,0 +1,16 @@
+app.factory('pageService', function() {
+    var currentPageName = '';
+
+    function getPageName() {
+        return currentPageName;
+    }
+
+    function setPageName(newName) {
+        currentPageName = newName;
+    }
+
+    return {
+        getName: getPageName,
+        setPageName: setPageName
+    }
+});
---
+++ app/js/services/userService.js
@@ -0,0 +1,40 @@
+app.factory('userService', function($resource, $http, baseUrl, authenticationService) {
+    $http.defaults.headers.common['Authorization'] = authenticationService.getHeaders().Authorization;
+    var userUrl = baseUrl + '/user',
+        userResource = $resource(
+            userUrl, null, {
+                update: {
+                    method: 'PUT'
+                },
+                getProfile: {
+                    url: userUrl + '/profile',
+                    method: 'GET'
+                },
+                edit: {
+                    url: userUrl + '/profile',
+                    method: 'PUT'
+                },
+                changePassword: {
+                    url: userUrl + '/changepassword',
+                    method: 'PUT'
+                }
+            });
+
+    function getUserData() {
+        return userResource.getProfile().$promise;
+    }
+
+    function editUserData(userData) {
+        return userResource.edit(userData).$promise;
+    }
+
+    function changePassword(passData) {
+        return userResource.changePassword(passData).$promise;
+    }
+
+    return {
+        getUser: getUserData,
+        editUser: editUserData,
+        changePassword: changePassword
+    }
+});
