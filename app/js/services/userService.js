b40da39b7191a711b7c73a62e3e3765db9e5b4df
dsmilyanov <dsmilyanov@gmail.com>
2015-01-11 12:52:16 +0200

    User service rework. Works on custom server.

--- app/js/services/userService.js
+++ app/js/services/userService.js
@@ -1,40 +1,103 @@
-app.factory('userService', function($resource, $http, baseUrl, authenticationService) {
-    $http.defaults.headers.common['Authorization'] = authenticationService.getHeaders().Authorization;
-    var userUrl = baseUrl + '/user',
-        userResource = $resource(
-            userUrl, null, {
-                update: {
-                    method: 'PUT'
-                },
-                getProfile: {
-                    url: userUrl + '/profile',
-                    method: 'GET'
-                },
-                edit: {
-                    url: userUrl + '/profile',
-                    method: 'PUT'
-                },
-                changePassword: {
-                    url: userUrl + '/changepassword',
-                    method: 'PUT'
-                }
-            });
+'use strict';
 
-    function getUserData() {
-        return userResource.getProfile().$promise;
-    }
+app.factory('userService',
+    function($http, baseServiceUrl, authService) {
+        return {
+            createNewAd: function(adData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/user/ads',
+                    headers: authService.getAuthHeaders(),
+                    data: adData
+                };
+                $http(request).success(success).error(error);
+            },
 
-    function editUserData(userData) {
-        return userResource.edit(userData).$promise;
-    }
+            getUserAds: function(params, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/user/ads',
+                    headers: authService.getAuthHeaders(),
+                    params: params
+                };
+                $http(request).success(success).error(error);
+            },
 
-    function changePassword(passData) {
-        return userResource.changePassword(passData).$promise;
-    }
+            deactivateAd: function(id, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/user/ads/deactivate/' + id,
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
 
-    return {
-        getUser: getUserData,
-        editUser: editUserData,
-        changePassword: changePassword
+            publishAgainAd: function(id, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/user/ads/publishAgain/' + id,
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
+
+            getAdInfo: function(id, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/user/ads/' + id,
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
+
+            deleteAd: function(id, success, error) {
+                var request = {
+                    method: 'DELETE',
+                    url: baseServiceUrl + '/api/user/ads/' + id,
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
+
+            editAd: function(adData, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/user/ads/' + adData.id,
+                    headers: authService.getAuthHeaders(),
+                    data: adData
+                };
+                $http(request).success(success).error(error);
+            },
+
+            getUserInfo: function(success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/user/profile',
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
+
+            editProfile: function(profileData, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/user/profile',
+                    headers: authService.getAuthHeaders(),
+                    data: profileData
+                };
+                $http(request).success(success).error(error);
+            },
+
+            changePassword: function(passwordData, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/user/changePassword',
+                    headers: authService.getAuthHeaders(),
+                    data: passwordData
+                };
+                $http(request).success(success).error(error);
+            }
+
+        }
     }
-});
+);
